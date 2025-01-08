a9cd8be3773378f032a88c3dd24f47e819ee196b
HyunSunAhn <kiwibird0417@gmail.com>
2025-01-08 13:21:59 +0900

    Class 부분 연습문제까지 업데이트...

--- Main.cpp
+++ Main.cpp
@@ -1,67 +1,100 @@
 #include <iostream>
 using namespace std;
 
-struct Robot {
+class CRobot {
+private:
 	string name;
 	int height;
 	int weight;
-	int magicpower;		//마력(MAGIC POWER 약어로 mp로 그냥 지정.)
+	int mp;
 
-	void Set(string n, int h, int w, int mp) {
+public:
+	void Set(string n, int h, int w, int m) {
 		name = n;
 		height = h;
 		weight = w;
-		magicpower = mp;
+		mp = m;
 	}
 
 	void Print() {
-		cout << "이름: " << name << endl;
-		cout << "신장: " << height << endl;
-		cout << "무게: " << weight << endl;
-		cout << "마력: " << magicpower << endl;
-		cout << endl;
+		printf("이름: %s, 신장: %d, 무게: %d, 마력: %d\n", name.c_str(), height, weight, mp);
 	}
-
 };
 
-void Avg(Robot robots[4]) {
-	float totalHeight = 0.0, totalWeight = 0.0, totalMP = 0.0;
-	float avgHeight = 0.0, avgWeight = 0.0, avgMP = 0.0;
+class CTest {
+private:
+	int s_Value;
 
-	for (int i = 0; i < 4; ++i) {
-		totalHeight += (float)robots[i].height;
-		totalWeight += (float)robots[i].weight;
-		totalMP += (float)robots[i].magicpower;
+public:
+	void Set(int g_Value) {
+		s_Value = g_Value;
 	}
+	void Print_S() {
+		printf("%d\n", s_Value);
+	}
+};
 
-	avgHeight = totalHeight / 4;
-	avgWeight = totalWeight / 4;
-	avgMP = totalMP / 4;
+class CTime {
+private:
+	int seconds;
+public:
+	void SetElapsed(int e) {
+		seconds = e;
+	}
+	void PrintTime() {
+		// 60초 = 1분
+		// 1시간 = 60분 = 3600초
+		int h= 0, m=0, s=0;
 
-	cout << "로봇들의 평균 신장: " << avgHeight << "m\n";
-	cout << "로봇들의 평균 무게: " << avgWeight << "T\n";
-	cout << "로봇들의 평균 마력: " << avgMP << "\n";
-}
+		if (seconds >= 3600) {
+			h = seconds / 3600;
+			m = (seconds - 3600) / 60;
+			s = seconds % 60;
+		}
+		else if (seconds >= 60) {
+			m = seconds / 60;
+			s = seconds % 60;
+		}
+		else {
+			s = seconds;
+		}
 
+		printf("%d시 %d분 %d초\n", h, m, s);
+
+	}
+};
 
 //=========================================================
 int main() {
-	Robot robots[4];
+	//(1)~(2)
+	CRobot robots[4];
 
-	//(1)로봇 나타내는 구조체 설계
 	robots[0].Set("태권브이", 18, 80, 3000);
 	robots[1].Set("마징가", 17, 70, 2500);
 	robots[2].Set("메칸더브이", 20, 90, 3500);
 	robots[3].Set("그랜다이져", 22, 100, 5000);
+	
+	robots[0].Print();
+	robots[1].Print();
+	robots[2].Print();
+	robots[3].Print();
+	//=================================================
+	//(3)
+	CTest test;
+	test.Set(1);
+	test.Print_S();
 
+	//=================================================
+	//(4)
+	cout << "자정부터 지나간 초를 입력하세요" << endl;
 
-	//(2)로봇 출력 Print
-	for (int i = 0; i < 4; ++i) {
-		robots[i].Print();
-	}
+	int Elapsed;
+	cin >> Elapsed;
 
-	//(3) 평균 출력
-	Avg(robots);
+	CTime t;
+	t.SetElapsed(Elapsed);
+	t.PrintTime();
+
 
 	return 0;
 }
